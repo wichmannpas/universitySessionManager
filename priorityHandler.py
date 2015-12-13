94340edfda15e92564b51b9540740fece5176ad9
Pascal Wichmann <wichmannpas@gmail.com>
2015-12-13 19:09:55 +0100

    added readme and license headers

--- priorityHandler.py
+++ priorityHandler.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+# Copyright 2015 Pascal Wichmann
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
 
 import sqlite3
 import itertools
